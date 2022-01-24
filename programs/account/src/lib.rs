7f64a4ff8c76dab2bb66a234325f961243dd4376
ZYJLiu <75003086+ZYJLiu@users.noreply.github.com>
2022-01-24 01:11:56 -0600

    redo List/Item
    item.amount += amount not working

--- programs/account/src/lib.rs
+++ programs/account/src/lib.rs
@@ -6,27 +6,48 @@ declare_id!("4kiCL5ZnfTViX7WddCYvGMowULgkJNrnAAWRoz3JVT7e");
 pub mod anchor {
     use super::*;
 
-    pub fn create(
-        ctx: Context<Create>,
+    pub fn create_list(
+        ctx: Context<CreateList>,
         name: String,
+        capacity: u16,
+        account_bump: u8,
     ) -> ProgramResult {
-        let item = &mut ctx.accounts.item;
-        let user: &Signer = &ctx.accounts.user;
-        item.name = name;
-
+        // Create a new account
+        let list = &mut ctx.accounts.list;
+        list.owner = *ctx.accounts.user.to_account_info().key;
+        list.bump = account_bump;
+        list.name = name;
+        list.capacity = capacity;
         Ok(())
     }
 
+    pub fn add_item(
+        ctx: Context<AddItem>,
+        _list_name: String,
+        item_name: String,
+    ) -> ProgramResult {
+        let list = &mut ctx.accounts.list;
+        let item = &mut ctx.accounts.item;
+        let user: &Signer = &ctx.accounts.user;
+        
+        list.lines.push(*item.to_account_info().key);
+        item.creator = *user.to_account_info().key;
+        item.name = item_name;
+        
+        Ok(())
+    }
 
+    //THIS IS WHERE IT IS STILL WORKING
+    
     pub fn pay(
         ctx: Context<Pay>,
         amount: u64,
     ) -> ProgramResult {
-        let item1 = &mut ctx.accounts.item1;
-        let item2 = &mut ctx.accounts.item2;
+        // let item1 = &mut ctx.accounts.item1;
+        // let item2 = &mut ctx.accounts.item2;
         let user: &Signer = &ctx.accounts.user;
-        item1.amount += amount;
-        item2.amount += amount;
+        // item1.amount += amount;
+        // item2.amount += amount;
 
 
         let ix = anchor_lang::solana_program::system_instruction::transfer(
@@ -75,68 +96,93 @@ pub mod anchor {
     //     Ok(())
     // }
 
-    pub fn receive(
-        ctx: Context<Receive>,
-        amount: u64,
-    ) -> ProgramResult {
-        // let item1 = &mut ctx.accounts.item1;
-        // let item2 = &mut ctx.accounts.item2;
-        let payer= &ctx.accounts.payer;
-        // item1.amount += amount;
-        // item2.amount += amount;
+    // pub fn receive(
+    //     ctx: Context<Receive>,
+    //     amount: u64,
+    // ) -> ProgramResult {
+    //     // let item1 = &mut ctx.accounts.item1;
+    //     // let item2 = &mut ctx.accounts.item2;
+    //     let payer= &ctx.accounts.payer;
+    //     // item1.amount += amount;
+    //     // item2.amount += amount;
 
 
-        let ix = anchor_lang::solana_program::system_instruction::transfer(
-            &ctx.accounts.payer.key(),
-            &ctx.accounts.user.key(),
-            amount * 1000000000,
-        );
-        anchor_lang::solana_program::program::invoke(
-            &ix,
-            &[
-                ctx.accounts.payer.to_account_info(),
-                ctx.accounts.user.to_account_info(),
-                ctx.accounts.system_program.to_account_info(),
-            ],
-        );
+    //     let ix = anchor_lang::solana_program::system_instruction::transfer(
+    //         &ctx.accounts.payer.key(),
+    //         &ctx.accounts.user.key(),
+    //         amount * 1000000000,
+    //     );
+    //     anchor_lang::solana_program::program::invoke(
+    //         &ix,
+    //         &[
+    //             ctx.accounts.payer.to_account_info(),
+    //             ctx.accounts.user.to_account_info(),
+    //             ctx.accounts.system_program.to_account_info(),
+    //         ],
+    //     );
 
-        Ok(())
-    }
+    //     Ok(())
+    // }
     
 }
 
-#[derive(Accounts)]
-#[instruction(amount: u64)]
-pub struct Receive<'info> {
-    // #[account(mut)]
-    // pub item1: Account<'info, DataAccount>,
-    // #[account(mut)]
-    // pub item2: Account<'info, DataAccount>,
-    #[account(mut)]
-    pub user: AccountInfo<'info>,
-    #[account(mut)]
-    pub payer: Signer<'info>,
-    pub system_program: Program<'info, System>,
+fn name_seed(name: &str) -> &[u8] {
+    let b = name.as_bytes();
+    if b.len() > 32 {
+        &b[0..32]
+    } else {
+        b
+    }
 }
 
 
+//Create "List" Below
 #[derive(Accounts)]
-#[instruction(amount: u64)]
-pub struct Pay<'info> {
-    #[account(mut)]
-    pub item1: Account<'info, DataAccount>,
-    #[account(mut)]
-    pub item2: Account<'info, DataAccount>,
-    #[account(mut)]
-    pub receiver: AccountInfo<'info>,
-    #[account(mut)]
+#[instruction(name: String, capacity: u16, list_bump: u8)]
+pub struct CreateList<'info> {
+    #[account(init,
+        payer=user,
+        space=List::space(&name, capacity),
+        seeds=[
+            b"list",
+            user.to_account_info().key.as_ref(),
+            name_seed(&name)
+        ],
+        bump=list_bump)]
+    pub list: Account<'info, List>,
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
+//"List" Data Account fields
+#[account]
+pub struct List {
+    pub owner: Pubkey,
+    pub name: String,
+    pub bump: u8,
+    pub capacity: u16,
+    pub lines: Vec<Pubkey>,
+}
+
+impl List {
+    fn space(name: &str, capacity: u16) -> usize {
+        // discriminator + owner pubkey + bump + capacity
+        8 + 32 + 1 + 2 +
+            // name string
+            4 + name.len() +
+            // vec of item pubkeys
+            4 + (capacity as usize) * std::mem::size_of::<Pubkey>()
+    }
+}
+
+//Create item
 #[derive(Accounts)]
-pub struct Create<'info> {
-    #[account(init, payer=user, space= 64)]
+#[instruction(list_name: String, item_name: String)]
+pub struct AddItem<'info> {
+    #[account(mut, has_one=owner, seeds=[b"list", owner.to_account_info().key.as_ref(), name_seed(&list_name)], bump=list.bump)]
+    pub list: Account<'info, List>,
+    pub owner: AccountInfo<'info>,
+    #[account(init, payer=user, space=DataAccount::space(&item_name))]
     pub item: Account<'info, DataAccount>,
     #[account(mut)]
     pub user: Signer<'info>,
@@ -146,9 +192,50 @@ pub struct Create<'info> {
 
 #[account]
 pub struct DataAccount{
+    pub creator: Pubkey,
     pub name: String,
     pub amount: u64,
 }
+
+impl DataAccount {
+    fn space(name: &str) -> usize {
+        // discriminator + name string + creator pubkey + amount?
+        8 + 4 + name.len() + 32 + 8
+    }
+}
+
+// Update Item "Pay"
+#[derive(Accounts)]
+#[instruction(amount: u64)]
+pub struct Pay<'info> {
+    #[account(mut)]
+    pub item1: Account<'info, DataAccount>,
+    #[account(mut)]
+    // pub item2: Account<'info, DataAccount>,
+    // #[account(mut)]
+    pub receiver: AccountInfo<'info>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+// #[derive(Accounts)]
+// #[instruction(amount: u64)]
+// pub struct Receive<'info> {
+//     // #[account(mut)]
+//     // pub item1: Account<'info, DataAccount>,
+//     // #[account(mut)]
+//     // pub item2: Account<'info, DataAccount>,
+//     #[account(mut)]
+//     pub user: AccountInfo<'info>,
+//     #[account(mut)]
+//     pub payer: Signer<'info>,
+//     pub system_program: Program<'info, System>,
+// }
+
+
+
+
 
 
         
--- tests/account.js
+++ tests/account.js
@@ -10,6 +10,8 @@ describe('Test', () => {
   anchor.setProvider(provider);
   const mainProgram = anchor.workspace.Anchor;
 
+  
+  //"User" functions
   async function createUser(airdropBalance) {
     airdropBalance = airdropBalance ?? 3 * LAMPORTS_PER_SOL;
     let user = anchor.web3.Keypair.generate();
@@ -35,13 +37,69 @@ describe('Test', () => {
     return Promise.all(promises);
   }
 
+  function programForUser(user) {
+    return new anchor.Program(mainProgram.idl, mainProgram.programId, user.provider);
+  }
+
   async function getAccountBalance(pubkey) {
     let account = await provider.connection.getAccountInfo(pubkey);
     return account?.lamports ?? 0;
   }
 
-  function programForUser(user) {
-    return new anchor.Program(mainProgram.idl, mainProgram.programId, user.provider);
+  //"List" function
+  async function createList(owner, name, capacity=16) {
+    const [listAccount, bump] = await anchor.web3.PublicKey.findProgramAddress([
+      "list",
+      owner.key.publicKey.toBytes(),
+      name.slice(0, 32)
+    ], mainProgram.programId);
+
+    let program = programForUser(owner);
+    await program.rpc.createList(name, capacity, bump, {
+      accounts: {
+        list: listAccount,
+        user: owner.key.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+    });
+
+    let list = await program.account.list.fetch(listAccount);
+    return { publicKey: listAccount, data: list };
+  }
+
+  //"Add Item" function
+  async function addItem({list, user, name }) {
+    const itemAccount = anchor.web3.Keypair.generate();
+    let program = programForUser(user);
+    await program.rpc.addItem(list.data.name, name, {
+      accounts: {
+        list: list.publicKey,
+        owner: list.data.owner,
+        item: itemAccount.publicKey,
+        user: user.key.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [
+        user.key,
+        itemAccount,
+      ]
+    });
+
+    let [listData, itemData] = await Promise.all([
+      program.account.list.fetch(list.publicKey),
+      program.account.dataAccount.fetch(itemAccount.publicKey),
+    ]);
+
+    return {
+      list: {
+        publicKey: list.publicKey,
+        data: listData,
+      },
+      item: {
+        publicKey: itemAccount.publicKey,
+        data: itemData,
+      }
+    };
   }
 
 
@@ -54,75 +112,113 @@ describe('Test', () => {
 
   it('Create Data Account', async () => {
     // Add your test here.
-    const item1 = anchor.web3.Keypair.generate();
-    const item2 = anchor.web3.Keypair.generate();
 
-    // const program = anchor.workspace.Anchor;
-
-    const [owner, payer, receiver] = await createUsers(3);
+    const owner = await createUser();
     const program = programForUser(owner);
-    const program2 = programForUser(payer);
 
-  
-    console.log(payer.key.publicKey.toString())
+    const revenue = await createList(owner, 'Revenue');
+    const expense = await createList(owner, 'Cash');
+    // let CashList = await createList(owner, 'Cash');
+    // console.log(list)
+    // console.log(ExpenseList)
+    // console.log(CashList)
+
+    const receiver = await createUser();
+
+    const item1 = await addItem({list: revenue, user: owner, name: 'Coffee',});
     
-    await program.rpc.create('Expense', {
+
+
+
+    // const item2 = await addItem({list: expense, user: owner, name: 'Cash',});
+    
+    // console.log(revenue.data.name)
+
+    // // const program2 = programForUser(receiver);
+    
+    
+    
+    const test = await program.rpc.pay(new anchor.BN(1), {
       accounts:{
-        item: item1.publicKey,
-        user: owner.key.publicKey,
-        systemProgram: anchor.web3.SystemProgram.programId,
-      },
-      signers: [item1]
-
-    });
-
-    await program.rpc.create('Accounts Payable', {
-      accounts:{
-        item: item2.publicKey,
-        user: owner.key.publicKey,
-        systemProgram: anchor.web3.SystemProgram.programId,
-      },
-      signers: [item2]
-
-    });
-
-    await program.rpc.pay(new anchor.BN(1), {
-      accounts:{
-        item1: item1.publicKey,
-        item2: item2.publicKey,
+        // list: revenue.publicKey,
+        // owner: owner.key.publicKey,
+        item1: item1.item.publicKey,
         receiver: receiver.key.publicKey,
-        systemProgram: anchor.web3.SystemProgram.programId,
         user: owner.key.publicKey,
-      },
-
-    });
-
-    await program2.rpc.receive(new anchor.BN(1), {
-      accounts:{
-        // item1: item1.publicKey,
-        // item2: item2.publicKey,
-        payer: payer.key.publicKey,
         systemProgram: anchor.web3.SystemProgram.programId,
-        user: owner.key.publicKey,
       },
+    
     });
-
-  
     
-    const Coffee = await program.account.dataAccount.fetch(item1.publicKey);
-    console.log(Coffee.amount);
 
-    const Cash = await program.account.dataAccount.fetch(item2.publicKey);
-    console.log(Cash.amount);
     
-    const balance = await getAccountBalance(payer.key.publicKey) / LAMPORTS_PER_SOL
-    console.log(balance);
+
+
 
     const balance1 = await getAccountBalance(receiver.key.publicKey) / LAMPORTS_PER_SOL
     console.log(balance1);
 
     const balance3 = await getAccountBalance(owner.key.publicKey) / LAMPORTS_PER_SOL
     console.log(balance3);
+    
+    console.log(item1) //convert to number
+    // console.log(item2)
+
+
+
+
+    
+    // const program = anchor.workspace.Anchor;
+
+    // const [owner, payer, receiver] = await createUsers(3);
+
+
+    // const item1 = anchor.web3.Keypair.generate();
+    // const item2 = anchor.web3.Keypair.generate();
+    // console.log(payer.key.publicKey.toString())
+    
+    // await program.rpc.create('Expense', {
+    //   accounts:{
+    //     item: item1.publicKey,
+    //     user: owner.key.publicKey,
+    //     systemProgram: anchor.web3.SystemProgram.programId,
+    //   },
+    //   signers: [item1]
+
+    // });
+
+    // await program.rpc.create('Accounts Payable', {
+    //   accounts:{
+    //     item: item2.publicKey,
+    //     user: owner.key.publicKey,
+    //     systemProgram: anchor.web3.SystemProgram.programId,
+    //   },
+    //   signers: [item2]
+
+    // });
+
+
+    // await program2.rpc.receive(new anchor.BN(1), {
+    //   accounts:{
+    //     // item1: item1.publicKey,
+    //     // item2: item2.publicKey,
+    //     payer: payer.key.publicKey,
+    //     systemProgram: anchor.web3.SystemProgram.programId,
+    //     user: owner.key.publicKey,
+    //   },
+    // });
+
+  
+    
+    // const Coffee = await program.account.dataAccount.fetch(item1.publicKey);
+    // console.log(Coffee.amount);
+
+    // const Cash = await program.account.dataAccount.fetch(item2.publicKey);
+    // console.log(Cash.amount);
+    
+    // const balance = await getAccountBalance(payer.key.publicKey) / LAMPORTS_PER_SOL
+    // console.log(balance);
+
 
 
 
